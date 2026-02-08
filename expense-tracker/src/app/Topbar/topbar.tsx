7974db900a95b80e91883b13c827ef281ab83c32
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2026-02-08 16:36:38 +0530

    last commit

--- expense-tracker/src/app/Topbar/topbar.tsx
+++ expense-tracker/src/app/Topbar/topbar.tsx
@@ -1,12 +1,11 @@
-"use client"
-import React from 'react';
-import {  Wallet } from 'lucide-react';
-import { useAuthStore } from '../store/authstore';
-
+"use client";
+import React from "react";
+import { Wallet } from "lucide-react";
+import { useAuthStore } from "../store/authstore";
 
 export function Topbar() {
   const { user } = useAuthStore();
-  console.log(user)
+  console.log(user);
 
   return (
     <nav className="bg-white  border-b border-slate-200  sticky top-0 z-40">
@@ -21,30 +20,21 @@ export function Topbar() {
           </div>
         </div>
         <div>
-          <h2 className='gradient-text w-fit text-3xl font-semibold'>Welcome back, {user?.name} !</h2>
-  </div>
+          <h2 className="gradient-text w-fit text-3xl font-semibold">
+            Welcome back, {user?.name} !
+          </h2>
+        </div>
         <div className="flex items-center gap-4">
-          {/* <button className="p-2 rounded-lg hover:bg-slate-100 dark:hover:bg-slate-700 transition-colors relative">
-            <Bell className="w-5 h-5 text-slate-600 dark:text-slate-400" />
-            <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full" />
-          </button>
-           */}
-          {/* <button
-            
-            className="p-2 rounded-lg hover:bg-slate-100  transition-colors"
-          >
-            
-              <Sun className="w-5 h-5 text-slate-400" />
-              <Moon className="w-5 h-5 text-slate-600" />
-          </button> */}
-
           <div className="w-10 h-10 rounded-full bg-teal-400 flex items-center justify-center text-white cursor-pointer">
-            <span> {user?.name
-      ?.split(" ")
-      .map(word => word[0])
-      .slice(0, 2)
-      .join("")
-      .toUpperCase()}</span>
+            <span>
+              {" "}
+              {user?.name
+                ?.split(" ")
+                .map((word) => word[0])
+                .slice(0, 2)
+                .join("")
+                .toUpperCase()}
+            </span>
           </div>
         </div>
       </div>
